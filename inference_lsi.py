3218005eeea1d32b2736f480d4c92129630acfeb
blevac <blevac@utexas.edu>
2023-11-29 13:20:08 -0600

    inference scripts for different inverse problems

---
+++ inference_lsi.py
@@ -0,0 +1,110 @@
+# boiler plate imports
+import numpy as np
+import glob
+import torch
+from tqdm import tqdm
+# import sigpy as sp
+import matplotlib.pyplot as plt
+import os
+import argparse
+from utils import nrmse, fft
+from sampling_funcs import StackedRandomGenerator, simple_ODE_ps, general_forward_SDE_ps
+import pickle
+import dnnlib
+from torch_utils import distributed as dist
+from skimage.metrics import structural_similarity as ssim
+from forwards import LSI_utils, gaussuian_filter
+# dist.init()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--l_ss', type=float, default=1)
+parser.add_argument('--sigma_max', type=float, default=10)
+parser.add_argument('--num_steps', type=int, default=300)
+parser.add_argument('--blur_sig', type=float, default=0.05)
+parser.add_argument('--seed', type=int, default=100)
+parser.add_argument('--discretization', type=str, default='edm') # ['vp', 've', 'iddpm', 'edm']
+parser.add_argument('--solver', type=str, default='euler') # ['euler', 'heun']
+parser.add_argument('--schedule', type=str, default='vp') # ['vp', 've', 'linear']
+parser.add_argument('--scaling', type=str, default='vp') # ['vp', 'none']
+
+args   = parser.parse_args()
+
+os.environ["CUDA_DEVICE_ORDER"]= "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]=str(args.gpu)
+#seeds
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+device=torch.device('cuda')
+
+
+# load sample 
+data_file = './test_data/test_cat.pt'
+gt_img = torch.load(data_file)
+
+
+results_dir = './lsi_results/'
+if not os.path.exists(results_dir):
+    os.makedirs(results_dir)
+
+# load network
+net_save = '/home/blevac/Diffusion_Inverse/models/edm-afhqv2-64x64-uncond-ve.pkl'
+
+if dist.get_rank() != 0:
+    torch.distributed.barrier()
+
+# Load network.
+dist.print0(f'Loading network from "{net_save}"...')
+with dnnlib.util.open_url(net_save, verbose=(dist.get_rank() == 0)) as f:
+    net = pickle.load(f)['ema'].to(device)
+
+
+
+# Pick latents and labels.
+batch_size=1
+rnd = StackedRandomGenerator(device, [args.seed])
+latents = rnd.randn([batch_size, net.img_channels, net.img_resolution, net.img_resolution], device=device)
+class_labels = None
+
+
+H = gt_img.shape[-2]
+W = gt_img.shape[-1]
+# psf = gaussuian_filter(kernel_size=gt_img.shape[-1],sigma=args.blur_sig)
+# psf = torch.tensor(psf)[None,None].cuda()
+psf = torch.tensor(torch.load('./test_data/psf_64x64.pt'))[None,None].cuda()
+# print(psf.shape)
+lsi_utils = LSI_utils(psf=psf,H=H,W=W)
+A_forw = lsi_utils.forward
+meas = A_forw(gt_img)
+
+recon_img = general_forward_SDE_ps(y=meas, A_forw=A_forw, task='', l_ss=args.l_ss, 
+    net=net, latents=latents, class_labels=None, randn_like=torch.randn_like,
+    num_steps=args.num_steps, sigma_min=0.002, sigma_max=args.sigma_max, rho=7,
+    solver=args.solver, discretization=args.discretization, schedule='linear', scaling=args.scaling,
+    epsilon_s=1e-3, C_1=0.001, C_2=0.008, M=1000, alpha=1,
+    S_churn=0, S_min=0, S_max=float('inf'), S_noise=1, gt_img=gt_img, verbose = True)
+
+
+img_nrmse = nrmse(gt_img, recon_img).item()
+
+# cplx_recon=cplx_recon.detach().cpu().numpy()
+# gt_img=gt_img.cpu().numpy()
+# img_SSIM = ssim(abs(gt_img[0,0]), abs(cplx_recon[0,0]), data_range=abs(gt_img[0,0]).max() - abs(gt_img[0,0]).min())
+
+
+# print('Sample %d, Seed %d, NRMSE: %.3f, SSIM: %.3f'%(args.sample,args.seed, img_nrmse, img_SSIM))
+
+dict = { 
+        'gt_img': gt_img,
+        'recon':recon_img,
+        'meas':meas,
+        'forward_utils':lsi_utils,
+        # 'img_stack': img_stack,
+        'nrmse':img_nrmse,
+        # 'ssim': img_SSIM 
+}
+
+torch.save(dict, results_dir + '/checkpoint.pt')
+
+
---
+++ inference_mri.py
@@ -0,0 +1,139 @@
+# boiler plate imports
+import numpy as np
+import glob
+import torch
+from tqdm import tqdm
+# import sigpy as sp
+import matplotlib.pyplot as plt
+import os
+import argparse
+from utils import nrmse, fft
+from sampling_funcs import StackedRandomGenerator, simple_ODE_ps, general_forward_SDE_ps
+import pickle
+import dnnlib
+from torch_utils import distributed as dist
+from skimage.metrics import structural_similarity as ssim
+from forwards import MRI_utils
+# dist.init()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--l_ss', type=float, default=1)
+parser.add_argument('--sigma_max', type=float, default=10)
+parser.add_argument('--num_steps', type=int, default=300)
+parser.add_argument('--ACS_perc', type=float, default=0.04)
+parser.add_argument('--R', type=int, default=4)
+parser.add_argument('--sample', type=int, default=0)
+parser.add_argument('--seed', type=int, default=0)
+parser.add_argument('--contrast_recon', type=str, default='PD')
+parser.add_argument('--net_arch', type=str, default='ddpmpp') 
+parser.add_argument('--discretization', type=str, default='edm') # ['vp', 've', 'iddpm', 'edm']
+parser.add_argument('--solver', type=str, default='euler') # ['euler', 'heun']
+parser.add_argument('--schedule', type=str, default='vp') # ['vp', 've', 'linear']
+parser.add_argument('--scaling', type=str, default='vp') # ['vp', 'none']
+
+args   = parser.parse_args()
+
+os.environ["CUDA_DEVICE_ORDER"]= "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]=str(args.gpu)
+#seeds
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+device=torch.device('cuda')
+
+
+# load sample 
+data_file = f'/csiNAS2/slow/brett/multicontrast_validation_data/fastMRI_knee/ACS_perc{args.ACS_perc}_R={args.R}/sample{args.sample}_R{args.R}.pt'
+contents = torch.load(data_file)
+# print(contents.keys())
+if args.contrast_recon == 'PD':
+    s_maps          = torch.tensor(contents['maps_1']).cuda() # shape: [1,C,H,W]
+    ksp           = torch.tensor(contents['ksp_1']).cuda()# shape: [1,C,H,W]
+    mask          = torch.tensor(contents['mask_1']).cuda() # shape: [1,1,H,W]
+    gt_img        = torch.tensor(contents['gt_img_1']).cuda() # shape [1,1,H,W]
+    norm_c        = torch.tensor(contents['norm1']).cuda() # scalar
+    class_idx = None
+
+# normalize undersampled kspace and ground truth image
+ksp = ksp/norm_c
+gt_img = gt_img/norm_c
+
+batch_size = 1
+
+
+results_dir = './results/'
+
+if not os.path.exists(results_dir):
+    os.makedirs(results_dir)
+
+# load network
+if args.contrast_recon=='PD':
+    net_save = '/csiNAS2/slow/brett/edm_outputs/00034-fastmri_PD_knee_preprocessed_7_21_23-uncond-ddpmpp-edm-gpus3-batch15-fp32-PD_knee_7_21_23/network-snapshot-010000.pkl'
+
+if dist.get_rank() != 0:
+        torch.distributed.barrier()
+
+# Load network.
+dist.print0(f'Loading network from "{net_save}"...')
+with dnnlib.util.open_url(net_save, verbose=(dist.get_rank() == 0)) as f:
+    net = pickle.load(f)['ema'].to(device)
+
+
+
+# Pick latents and labels.
+rnd = StackedRandomGenerator(device, [args.seed])
+latents = rnd.randn([batch_size, net.img_channels, net.img_resolution, net.img_resolution], device=device)
+class_labels = None
+
+
+# image_recon, img_stack = simple_ODE_ps(net=net, gt_img=gt_img, y=ksp, maps=s_maps, mask=mask, 
+#                                 latents=latents, l_ss=args.l_ss, class_labels=None,
+#                                 num_steps=args.num_steps, sigma_min=0.002, 
+#                                 sigma_max=args.sigma_max, rho=7)
+
+# def A_forw(x):
+#     #image shape: [B,1,H,W]
+#     #maps shape:  [B,C,H,W]
+#     # mask shape: [B,1,H,W]
+#     x = torch.view_as_complex(x.permute(0,-2,-1,1).contiguous())[None]
+#     coil_imgs = s_maps*x
+#     coil_ksp = fft(coil_imgs)
+#     sampled_ksp = mask*coil_ksp
+#     return sampled_ksp
+mri_utils = MRI_utils(maps=s_maps,mask=mask)
+A_forw = mri_utils.forward
+
+image_recon = general_forward_SDE_ps(y=ksp, A_forw=A_forw, task='mri', l_ss=args.l_ss, 
+    net=net, latents=latents, class_labels=None, randn_like=torch.randn_like,
+    num_steps=args.num_steps, sigma_min=0.002, sigma_max=args.sigma_max, rho=7,
+    solver=args.solver, discretization=args.discretization, schedule='linear', scaling=args.scaling,
+    epsilon_s=1e-3, C_1=0.001, C_2=0.008, M=1000, alpha=1,
+    S_churn=0, S_min=0, S_max=float('inf'), S_noise=1, gt_img=gt_img, verbose = True)
+
+
+cplx_recon = torch.view_as_complex(image_recon.permute(0,-2,-1,1).contiguous())[None] #shape: [1,1,H,W]
+#unnormalize
+cplx_recon = cplx_recon*norm_c
+gt_img = gt_img*norm_c
+
+img_nrmse = nrmse(abs(gt_img), abs(cplx_recon)).item()
+
+cplx_recon=cplx_recon.detach().cpu().numpy()
+gt_img=gt_img.cpu().numpy()
+img_SSIM = ssim(abs(gt_img[0,0]), abs(cplx_recon[0,0]), data_range=abs(gt_img[0,0]).max() - abs(gt_img[0,0]).min())
+
+
+print('Sample %d, Seed %d, NRMSE: %.3f, SSIM: %.3f'%(args.sample,args.seed, img_nrmse, img_SSIM))
+
+dict = { 
+        'gt_img': gt_img,
+        'recon':cplx_recon,
+        # 'img_stack': img_stack,
+        'nrmse':img_nrmse,
+        'ssim': img_SSIM 
+}
+
+torch.save(dict, results_dir + '/checkpoint.pt')
+
+
