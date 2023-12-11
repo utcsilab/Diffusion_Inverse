da017b2296ca9b660137d6c95149a0b0f5fe94a6
blevac <blevac@utexas.edu>
2023-12-11 15:01:09 -0600

    add ability to do batch solving

--- forwards.py
+++ forwards.py
@@ -68,8 +68,60 @@ class CS_utils:
 
     def forward(self,x):
         #image shape: [B,C,H,W]
+        # print('x shape: ', x.shape)
         x_vec = torch.reshape(x,(self.batch, self.chan, self.n))
+        # print('A shape: ', self.A.shape)
+        # print('x_vec shape:  ',x_vec.shape)
         sampled_meas = torch.matmul(x_vec, self.A)      
+        #meas shape: [B,C,m]
+        return sampled_meas
+
+    def meas_forward(self,x):
+        #image shape: [B,C,H,W]
+        # print('x shape: ', x.shape)
+        x_vec = torch.reshape(x,(1, self.chan, self.n))
+        # print('A shape: ', self.A.shape)
+        # print('x_vec shape:  ',x_vec.shape)
+        sampled_meas = torch.matmul(x_vec, self.A)      
+        #meas shape: [B,C,m]
+        return sampled_meas
+
+    def adjoint(self,y):
+        #input shape: [B,C,m]
+
+        adj_vector = torch.matmul(y, self.A.T)
+        adj_img    = torch.reshape(adj_vector, (self.batch, self.chan, self.H, self.W))       
+        return adj_img
+
+
+class CS_blind_utils:
+    def __init__(self, m, H, W, error_scale=0, batch=1, chan=3, device='cuda'):
+        self.m = m
+        self.H = H
+        self.W = W
+        self.n = H*W
+        self.chan = chan
+        self.batch = batch
+        self.A = torch.randn(self.n, self.m,dtype=torch.double,device=device)/self.m**.5
+        self.A_error = (torch.randn(self.n, self.m,dtype=torch.double,device=device)/self.m**.5) * error_scale
+
+    def assumed_forward(self,x):
+        #image shape: [B,C,H,W]
+        # print('x shape: ', x.shape)
+        x_vec = torch.reshape(x,(self.batch, self.chan, self.n))
+        # print('A shape: ', self.A.shape)
+        # print('x_vec shape:  ',x_vec.shape)
+        sampled_meas = torch.matmul(x_vec, self.A)      
+        #meas shape: [B,C,m]
+        return sampled_meas
+
+    def true_forward(self,x):
+        #image shape: [B,C,H,W]
+        # print('x shape: ', x.shape)
+        x_vec = torch.reshape(x,(self.batch, self.chan, self.n))
+        # print('A shape: ', self.A.shape)
+        # print('x_vec shape:  ',x_vec.shape)
+        sampled_meas = torch.matmul(x_vec, self.A) + torch.matmul(x_vec, self.A_error)      
         #meas shape: [B,C,m]
         return sampled_meas
 
@@ -90,13 +142,13 @@ class InPaint_utils:
         self.batch = batch
         idx = torch.arange(self.H*self.W)
         idx=np.random.permutation(idx)
-        self.A = torch.zeros((self.batch,1,self.H*self.W), dtype=torch.double,device=device)
+        self.A = torch.zeros((1,1,self.H*self.W), dtype=torch.double,device=device)
         self.A[:,:,idx[0:m]] = 1
-        self.A = self.A.reshape(self.batch,1,self.H,self.W)
+        self.A = self.A.reshape(1,1,self.H,self.W)
     def forward(self,x):
         #image shape: [B,C,H,W]
         sampled_meas = self.A*x   
-        #meas shape: [B,C,m]
+        #meas shape: [B,C,H,W]
         return sampled_meas
 
     def adjoint(self,y):
@@ -126,17 +178,11 @@ class LSI_utils:
         #image shape: [B,C,H,W]
         psf_pad = self.pad(self.psf)
         x_pad   = self.pad(x)
-        # psf_pad = self.psf
-        # x_pad = x
+        psf_pad = self.psf
+        x_pad = x
         x_fft = fft(x_pad,norm='backward')
         psf_fft = fft(psf_pad,norm='backward')
 
         meas_fft = psf_fft*x_fft
         meas_pad = ifft(meas_fft,norm='backward')  
         return self.crop(meas_pad)
-
-    def adjoint(self,y):
-        #input shape: [B,C,m]
-
-        adj_img = self.A*y         
-        return adj_img
